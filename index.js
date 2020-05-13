9d9c4e43f1da0355e0026b38bdf1ac5767fedd1f
eshwarmuthu <eshwarnataraj98@gmail.com>
2020-05-14 00:38:54 +0530

    mongodb added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const fetch=require("node-fetch")
+const bodyParser=require("body-parser")
+const mongoutil=require("./mongoUtils")
 // const input=require("./inputFromCLI")
 
 const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 app.set("views","./views")
 app.set("view engine","pug")
+mongoutil.connectDBServer()
 const baseURL="https://api.stackexchange.com/2.2"
 
 app.get("/",(req,res)=>{
@@ -13,13 +18,19 @@ app.get("/",(req,res)=>{
     res.render("index",{usingPug:true})
 })
 app.get("/getUserData",(req,res)=>{
-    var userID=req.query.userID
+    var userURL=req.query.userURL
+    var userID=userURL.split("/")[4]
+    var db=mongoutil.getDB()
+    db.collection("users").insertOne({userID:userID})
     var url=baseURL+"/users/"+userID+"/badges?site=stackoverflow"
-    console.log(url)
     fetch(url).then(resp=>resp.json())
     .then(data=>{
         console.log(data.items[0].user.display_name)
         res.send(data.items[0].user.display_name)
     })
+})
+app.post("/save",(req,res)=>{
+    // console.log(req.body)
+    res.send("save")
 })
 app.listen(8080)
---
+++ mongoUtils.js
@@ -0,0 +1,10 @@
+const MongoClient=require("mongodb").MongoClient
+var db;
+function connectDBServer(){
+    MongoClient.connect("mongodb://127.0.0.1:27017",(err,client)=>{
+        db=client.db("expand-db")
+    })
+}
+function getDB(){return db}
+module.exports.connectDBServer=connectDBServer
+module.exports.getDB=getDB
